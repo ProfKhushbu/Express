526b4a7914e77114e6214f67a04a69e8b17b5421
ProfKhushbu <143784535+ProfKhushbu@users.noreply.github.com>
2025-05-05 21:49:51 +0530

    Add files via upload

--- apit.js
+++ apit.js
@@ -5,3 +5,6 @@ app.use("/api", api);
 app.listen(7899, () => {
 console.log("Server running at http://localhost:7899/api");
 });
+// http://localhost:7899/api/player/id/1or2 or3 to fetch playerdetail based on id
+
+//http://localhost:7899/api/player/role/ batsman or bowler to fetch playerdetail based on role
